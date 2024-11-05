c58a64d059db005234fae7090f2eff0600c7c031
Ash <4707734+Eiad@users.noreply.github.com>
2024-11-05 19:31:14 +0400

    updates to media feature

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
 import '@/styles/globals.scss';
+import '@fontsource/caveat';
 import '@fontsource/poppins';
 
 export default function App({ Component, pageProps }) {
